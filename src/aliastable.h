1a67244d7f4bf584efab59082a6864484c636c06
Reed A. Cartwright <reed@scit.us>
2013-01-04 22:18:45 -0700

    added update aliastable.h which contains two bug fixes

--- src/aliastable.h
+++ src/aliastable.h
@@ -2,27 +2,26 @@
 #ifndef ALIASTABLE_H
 #define ALIASTABLE_H
 
-#include <boost/cstdint.hpp>
-#include <boost/range.hpp>
-#include <boost/range/numeric.hpp>
-
+#include <cstdint>
 #include <vector>
+#include <numeric>
+#include <cassert>
 
 class alias_table {
 public:
-	typedef boost::uint64_t uint64;
-	typedef boost::uint32_t uint32;
+	typedef uint64_t uint64;
+	typedef uint32_t uint32;
 	
 	alias_table() { }
-	template< typename T >
 	
+	template< typename T >
 	explicit alias_table(const T &v) {
 		create(v);
 	}
 	
 	uint32 get(uint64 u) const {
-		uint32 x = static_cast<uint32>(u);
-		uint32 y = static_cast<uint32>(u >> shr);
+		uint32 x = static_cast<uint32>(u >> shr);
+		uint32 y = static_cast<uint32>(u);
 		return ( y < p[x]) ? x : a[x];
 	}
 	
@@ -30,82 +29,19 @@ public:
 		return get(u);
 	}
 	
-	
 	// create the alias table
+	//template< typename T >
+	//void create(const T &v) {
+	//	std::vector<double> vv(boost::begin(v),boost::end(v));
+	//	create_inplace(vv);
+	//}
+
 	template< typename T >
-	void create(const T &v) {
-		std::vector<double> vv(boost::begin(v),boost::end(v));
+	void create(T first, T last) {
+		std::vector<double> vv(first,last);
 		create_inplace(vv);
 	}
 	
-	// create the alias table
-	void create_inplace2(std::vector<double> &v) {
-		assert(v.size() <= std::numeric_limits<uint32>::max());
-		// round the size of vector up to the nearest power of two
-		std::pair<std::vector<double>::size_type,int> ru = round_up(v.size());
-		const std::vector<double>::size_type sz = ru.first;
-		v.resize(sz,0.0);
-		a.resize(sz,0);
-		p.resize(sz,0);
-		// use the number of bits to calculate the right shift operand
-		shr = 64 - ru.second;
-		
-		// rescale input vector
-		double d = boost::accumulate(v,0.0);
-		d = sz/d;
-		for(int i=0;i<sz;++i)
-			v[i] = v[i]*d;
-		
-		// find first large and small values
-		//     g: current large value index
-		//     m: current small value index
-		//    mm: next possible small value index
-		std::vector<double>::size_type g,m,mm;
-		for(g=0; g<sz && v[g] <  1.0; ++g)
-			/*noop*/;
-		for(m=0; m<sz && v[m] >= 1.0; ++m)
-			/*noop*/;
-		mm = m+1;
-		
-		// contruct table
-		while(g < sz && m < sz) {
-			assert(v[m] < 1.0);
-			p[m] = static_cast<uint32>(4294967296.0*v[m]);
-			a[m] = static_cast<uint32>(g);
-			v[g] = (v[g]+v[m])-1.0;
-			if(v[g] >= 1.0 || mm < g) {
-				for(m=mm;m<sz && v[m] >= 1.0; ++m)
-					/*noop*/;
-				mm = m+1;
-			} else
-				m = g;
-			for(; g<sz && v[g] <  1.0; ++g)
-				/*noop*/;
-		}
-		// if we stopped early fill in the rest
-		if(g < sz) {
-			p[g] = std::numeric_limits<uint32>::max();
-			a[g] = static_cast<boost::uint32_t>(g);
-			for(g=g+1; g<sz; ++g) {
-				if(v[g] < 1.0)
-					continue;
-				p[g] = std::numeric_limits<uint32>::max();
-				a[g] = static_cast<uint32>(g);
-			}
-		}
-		// if we stopped early fill in the rest
-		if(m < sz) {
-			p[m] = std::numeric_limits<uint32>::max();
-			a[m] = static_cast<uint32>(m);
-			for(m=mm; m<sz; ++m) {
-				if(v[m] > 1.0)
-					continue;
-				p[m] = std::numeric_limits<uint32>::max();
-				a[m] = static_cast<boost::uint32_t>(m);
-			}
-		}
-	}
-
 	// create the alias table
 	void create_inplace(std::vector<double> &v) {
 		assert(v.size() <= std::numeric_limits<uint32>::max());
@@ -119,7 +55,7 @@ public:
 		shr = 64 - ru.second;
 		
 		// find scale for input vector
-		double d = boost::accumulate(v,0.0)/sz;
+		double d = std::accumulate(v.begin(),v.end(),0.0)/sz;
 		
 		// find first large and small values
 		//     g: current large value index
@@ -138,7 +74,7 @@ public:
 			p[m] = static_cast<uint32>(4294967296.0/d*v[m]);
 			a[m] = static_cast<uint32>(g);
 			v[g] = (v[g]+v[m])-d;
-			if(v[g] >= d || mm < g) {
+			if(v[g] >= d || mm <= g) {
 				for(m=mm; m<sz && v[m] >= d; ++m)
 					/*noop*/;
 				mm = m+1;
@@ -171,7 +107,7 @@ public:
 		}
 	}
 	
-//private:
+private:
 	template<typename T>
 	inline static std::pair<T,int> round_up(T x) {
 		T y = static_cast<T>(2);
